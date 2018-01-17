48e15435a0e87571b39682bb3fb8c262406a5994
Alex Brown <alexbrownfour@gmail.com>
2018-01-17 07:05:34 +0000

    Added partials and pages Auto reload dev

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const request = require('request');
+
+const http = require('http')
+const path = require('path')
+
+const reload = require('reload');
+const logger = require('morgan');
+
 const app = express()
 
 const apiKey = 'pooch';
@@ -12,25 +19,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-	res.render('index', {dummy_variable: null, error: null});
-})
+		var title = "Barbs & Auds Hats and Facinators"
+		var tagline = "Bespoke Hats and Facinators"
+	res.render('pages/index', {
+			title: title, 
+			tagline: tagline,
+			error: null
+			});
+});
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
 app.post('/', function (req, res) {
   let dummy_input = req.body.dummy_input;
   let url = ``
 request(url, function (err, response, body) {
     if(err){
-      res.render('index', {dummy_variable: null, error: 'Error, please try again'});
+      res.render('pages/index', {dummy_variable: null, error: 'Error, please try again'});
     } else {
       let dummy_variable = JSON.parse(body)
       if(dummy_variable == undefined){
-        res.render('index', {dummy_variable: null, error: 'Error, please try again'});
+        res.render('pages/index', {dummy_variable: null, error: 'Error, please try again'});
       } else {
         let dummy_text = `It's ${dummy_variable.main.dummy} text ${dummy_variable.name}!`;
-        res.render('index', {dummy_variable: dummy_text, error: null});
+        res.render('pages/index', {dummy_variable: dummy_text, error: null});
       }
     }
   });
 })
-app.listen(3000, function () {
+
+const server = http.createServer(app)
+
+// Reload script
+reload(app)
+
+
+server.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
