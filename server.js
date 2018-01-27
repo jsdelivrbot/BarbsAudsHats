cf4d18d5ba0489cefa7fbaee52c9839fe1cda57d
Alex Brown <alexbrownfour@gmail.com>
2018-01-27 19:09:05 +0000

    Responsive layout & styles added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const logger = require('morgan');
 
 const app = express()
 
+
 const apiKey = 'pooch';
 
 app.use(express.static('public'));
@@ -20,7 +21,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
 		var title = "Barbs & Auds Hats and Facinators"
-		var tagline = "Bespoke Hats and Facinators"
+		var tagline = "Bespoke hats & facinators tailored to the event you're lucky enough to be attending"
 	res.render('pages/index', {
 			title: title, 
 			tagline: tagline,
@@ -29,6 +30,9 @@ app.get('/', function (req, res) {
 });
 app.get('/about', function(req, res) {
 	res.render('pages/about');
+});
+app.get('/facinators', function(req, res) {
+	res.render('pages/facinators');
 });
 app.post('/', function (req, res) {
   let dummy_input = req.body.dummy_input;
@@ -54,6 +58,6 @@ const server = http.createServer(app)
 reload(app)
 
 
-server.listen(3000, function () {
+server.listen(3000, '0.0.0.0',function () {
   console.log('Example app listening on port 3000!')
 })
